e5ddeb78f03274ace29d9628add84a01aca3fca8
Grace Maretsky <gmaretsky@outlook.com>
2026-04-21 16:16:48 -0400

    Update ddapp.py

--- ddapp.py
+++ ddapp.py
@@ -592,7 +592,7 @@ st.download_button(
     file_name="diamond_dynamics_report.png",
     mime="image/png"
 )
-        st.markdown(f"""
+st.markdown(f"""
         <div class="insight-box">
             <b>Profile Summary</b><br><br>
             {build_summary(consistency, adjustment, baseline, value_col)}
