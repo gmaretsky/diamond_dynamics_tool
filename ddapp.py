e8d382554078f3a130493a3b111b686df3c9318c
Grace Maretsky <gmaretsky@outlook.com>
2026-04-21 20:35:50 -0400

    Update ddapp.py

--- ddapp.py
+++ ddapp.py
@@ -571,7 +571,7 @@ st.markdown(f"""
         """, unsafe_allow_html=True)
 
 st.subheader("Consistency Trend")
-        plot_line(
+plot_line(
             x=trend_x_labels,
             y=trend_df["Consistency"],
             title="Consistency Trend",
